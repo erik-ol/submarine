9f8f5304c41641c8b90dc940bbaf06ef51fb7c43
nataliie01 <natalie.2001.eriksson@hotmail.com>
2026-09-15 16:32:30 +0200

    Added code for potentiometer input

--- Arduino/micUp/micUp.ino
+++ Arduino/micUp/micUp.ino
@@ -1,24 +1,36 @@
 const int micPin = A0; 
-int maxVal = 0; 
-int count = 0;
+const int potPin = A3;
+int micMaxVal = 0; 
+int micCount = 0;
+int potLastVal = 0; 
 
 void setup() {
   Serial.begin(9600);
   pinMode(micPin, INPUT);
+  pinMode(potPin, INPUT);
 }
 
 void loop() {
   int micValue = analogRead(micPin);
-  if (micValue>maxVal){
-    maxVal = micValue;
-    Serial.println(maxVal);
+  if (micValue>micMaxVal){
+    micMaxVal = micValue;
+    Serial.print("Microphone: ");
+    Serial.println(micMaxVal);
   }
   else {
-    count ++;
-    if(count == 10) {
-      maxVal = maxVal-1;
-      count = 0;
+    micCount ++;
+    if(micCount == 10) {
+      micMaxVal = micMaxVal-1;
+      micCount = 0;
     }
+  }
+
+  int potValue = analogRead(potPin);
+  if (potValue != potLastVal)
+  {
+    Serial.print("Potentiometer: ");
+    Serial.println(potValue);
+    potLastVal = potValue;
   }
 
 }
--- UnderPressure/Assets/Scripts/ArduinoConnector.cs
+++ UnderPressure/Assets/Scripts/ArduinoConnector.cs
@@ -3,19 +3,30 @@ using System.IO.Ports;
 using System;
 
 
+
 public class ArduinoConnector : MonoBehaviour
 {
     SerialPort serial = new SerialPort("COM5", 9600);   
     int micBaseline;
     Vector3 initialPosition;
-    float sinkSpeed = 5f; 
-    float sensitivity = 5f; // Adjust this value to change the sensitivity of the movement
+    float sinkSpeed = 6f; 
+    float sensitivity = 2f; // Adjust this value to change the sensitivity of the movement
     void Start()
     { 
         serial.Open();
         serial.ReadTimeout = 50;
-        micBaseline = int.Parse(serial.ReadLine());
-        Debug.Log("baseline: " +micBaseline);
+        string initalData = serial.ReadLine();
+        string [] initialInput = initalData.Split(':');
+        if (initialInput[0] == "Microphone")
+        {
+            micBaseline = int.Parse(initialInput[1]);
+            Debug.Log("baseline: " +micBaseline);  
+        }
+        else
+        {
+            micBaseline = 700;
+            Debug.Log("No baseline found: " +micBaseline);  
+        }
         initialPosition = transform.position;
     }
 
@@ -32,27 +43,65 @@ public class ArduinoConnector : MonoBehaviour
         {
             return;
         }
-        //turning the value into int
-        int value = int.Parse(data);
-        Debug.Log("value: " + value);
-       if (value > micBaseline + 100)
-        {
-            
-            float yOffset = value*sensitivity/1023f;
-            Debug.Log("yOffset: " + yOffset);
-            transform.position +=  new Vector3(0, yOffset, 0); //the 100 is the range for height, so we change this depending on the depth of the water
-        }
 
-        if (transform.position.y > initialPosition.y) // slowly go down to the initial hight
-        {
-            Vector3 target = new Vector3(transform.position.x, initialPosition.y, transform.position.z);
-            transform.position = Vector3.MoveTowards(transform.position, target, sinkSpeed*Time.deltaTime);
+        //splitting the string into what type of input it is
+        string[] input = data.Split(':'); //e.g. input "microphone: 832"
 
-            // snap once close enough
-            if (Mathf.Abs(transform.position.y - initialPosition.y) < 0.001f)
+        if (input[0] == "Microphone")
+        {
+
+            //turning the value into int
+            int value = int.Parse(input[1]);
+            Debug.Log("micvalue: " + value);
+            if (value > micBaseline + 100)
             {
-                transform.position = target;
+                    
+                    float yOffset = value*sensitivity/1023f;
+                    Debug.Log("yOffset: " + yOffset);
+                    
+                    transform.position +=  new Vector3(0, yOffset, 0); //the 100 is the range for height, so we change this depending on the depth of the water
+            }
+
+            if (transform.position.y > initialPosition.y) // slowly go down to the initial hight
+            {
+                Vector3 target = new Vector3(transform.position.x, initialPosition.y, transform.position.z);
+                transform.position = Vector3.MoveTowards(transform.position, target, sinkSpeed*Time.deltaTime);
+
+                // snap once close enough
+                if (Mathf.Abs(transform.position.y - initialPosition.y) < 0.001f)
+                {
+                    transform.position = target;
+                }
             }
         }
+
+        if (input[0] == "Potentiometer")
+        {
+            //turning the value into int
+            int value = int.Parse(input[1]);
+            Debug.Log("potvalue: " + value);
+            if (value > micBaseline + 100)
+            {
+                    
+                    float yOffset = value*sensitivity/1023f;
+                    Debug.Log("yOffset: " + yOffset);
+                    
+                    transform.position -=  new Vector3(0, yOffset, 0); //the 100 is the range for height, so we change this depending on the depth of the water
+            }
+
+            if (transform.position.y < initialPosition.y) // slowly go down to the initial hight
+            {
+                Vector3 target = new Vector3(transform.position.x, initialPosition.y, transform.position.z);
+                transform.position = Vector3.MoveTowards(transform.position, target, sinkSpeed*Time.deltaTime);
+
+                // snap once close enough
+                if (Mathf.Abs(transform.position.y - initialPosition.y) < 0.001f)
+                {
+                    transform.position = target;
+                }
+            }
+            
+        }
+        
     }
 }
