8c574049b4489b1739cc940c403ea6e0b2284f90
nataliie01 <natalie.2001.eriksson@hotmail.com>
2026-09-17 16:08:44 +0200

    Update micUp.ino

--- Arduino/micUp/micUp.ino
+++ Arduino/micUp/micUp.ino
@@ -1,12 +1,7 @@
 const int micPin = A0; 
-<<<<<<< Updated upstream
-int maxVal = 0; 
-int count = 0;
-=======
 const int potPin = A3;
 int micMaxVal = 0; 
 int micCount = 0;
->>>>>>> Stashed changes
 
 void setup() {
   Serial.begin(9600);
@@ -27,11 +22,8 @@ void loop() {
     }
   }
 
-<<<<<<< Updated upstream
-=======
   int potValue = analogRead(potPin);
   Serial.print("Potentiometer: ");
   Serial.println(potValue);
 
->>>>>>> Stashed changes
 }
